fed3950a787dee7c9404aaf952699107edfdee43
harshitmasiwal <hmasiwal15@gmail.com>
2024-12-12 15:31:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,111 @@
+// function hello(){
+//     document.getElementById("para").innerHTML="hellllllllo";
+
+//     alert("hello")
+// }
+
+// var per = "hello"
+// var pi = 3.14
+
+// document.write(pi + "<br>")
+// document.write(per + "<br>")
+
+// console.log(typeof(pi))
+
+
+// var person = {
+//     firstname : "john",
+//     lastname : "mki",
+
+//     id : "5566",
+
+//     getinfo :function(){
+//         return this.firstname + " "+ this.lastname;
+//     }
+// }
+
+
+// var person2 = new Object();
+// person2.name = "krishna";
+// person2.age = 32;
+// person2.marrage = true;
+// person2.address = {};
+// person2.address.street = "nagar gali"
+// // person2.address['address']['flat'] = 33;    
+// console.log(person2.name);
+// document.write(person2['name'])
+// document.write("<br>")
+// document.write(person2['age'])
+
+// document.write("<br>")
+// console.log(person2.name);
+
+
+// function fun(){
+//     var carname = "volo"
+//     document.getElementById("demo").innerHTML = typeof carname+" "+carname;
+
+// }
+
+// fun();
+
+// var con={
+// europe:function(){
+//     var myfriend = "hallajuh";
+
+// var france = function(){
+// var paris = function(){
+//     console.log(myfriend)
+// }
+// paris();
+// }
+// france();
+// }}
+// con.europe()
+
+
+// function show(){
+//     var out = "bhar hu";
+//     document.write("out + <br>")
+
+//     function inside(){
+//         var inside = "andr hu";
+//         document.write("inside + <br>")
+//     }
+//     inside();
+
+// }
+// show();
+
+
+
+// var name = 'jhon';
+
+// function first(){
+//     var a = "h";
+//     sec();
+//     var x = a + name;
+
+// }
+
+// function sec(){
+//     var b = "hi";
+//     third();
+//     var z = 'b';
+// }
+
+// function third(){
+//     var c = "may";
+//     var z = c + "name";
+
+// }
+
+// first();
+
+
+// var age = 20 ;
+// if(age > 18 ){
+//     document.write("<b>you are 18+</b>");
+// }
+
+// document.getElementById("demo2").innerHTML = typeof carname;
