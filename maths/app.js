667628632f67ae40c0abfeb529b719ce5cf02b7d
harshitmasiwal <hmasiwal15@gmail.com>
2025-01-09 12:16:33 +0530

    math

---
+++ maths/app.js
@@ -0,0 +1,65 @@
+// console.log(Math.ceil(4.9))
+// console.log(Math.ceil(-3.5))
+// console.log(Math.ceil(0))
+// console.log(Math.ceil(1.2))
+// console.log(Math.ceil(-4.2))
+// console.log(Math.ceil(4.7))
+
+
+// console.log(Math.floor(4.9))
+// console.log(Math.floor(-3.5))
+// console.log(Math.floor(0))
+// console.log(Math.floor(1.2))
+// console.log(Math.floor(-4.2))
+// console.log(Math.floor(4.7))
+
+// console.log(Math.trunc(4.9))
+// console.log(Math.trunc(-3.5))
+// console.log(Math.trunc(0))
+// console.log(Math.trunc(1.2))
+// console.log(Math.trunc(-4.2))
+// console.log(Math.trunc(4.7))
+
+// console.log(Math.round(4.9))
+// console.log(Math.round(-3.5))
+// console.log(Math.round(0))
+// console.log(Math.round(1.2))
+// console.log(Math.round(-4.2))
+// console.log(Math.round(4.7))
+
+
+// console.log(Math.sign(0))
+// console.log(Math.sign(1))
+// console.log(Math.sign(-1))
+// console.log(Math.pow(2,4))
+// console.log(Math.sqrt(2))
+// console.log(Math.abs(2))            
+
+// console.log(Math.max(2,10,22))
+// console.log(Math.min(2,10,22))
+
+
+
+// console.log(Number("3.14"))
+// console.log(Number())
+// console.log(Number(""))
+// console.log(Number(" "))
+
+let y = "jhon"
+let x = +y;
+
+// x.toString()
+// (123).toString()
+
+d = new Date();
+Number(d)
+console.log(Number(d))
+
+
+
+
+
+
+
+
+
